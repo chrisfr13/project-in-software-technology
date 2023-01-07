012144238429945c6b5370e83287e0fcf6dcb8bd
CHRISTOS PAPANTONOPOULOS <88137772+chrisfr13@users.noreply.github.com>
2023-01-07 10:44:30 +0200

    Delete NewApiLatest directory

--- NewApiLatest/src/main/java/spring/api/news/repository/CommentRepository.java
+++
@@ -1,9 +0,0 @@
-package spring.api.news.repository;
-
-import spring.api.news.model.News;
-import org.springframework.data.repository.CrudRepository;
-
-
-public interface CommentRepository extends CrudRepository<News,Long> {
-
-}
